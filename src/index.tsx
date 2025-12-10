29b0ac84ed481597925dc7af567a3c0b0f8b4906
Shaquille <shaqdulove@hotmail.com>
2025-12-10 12:49:03 -0400

    export bottom sheet portal

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,8 @@
+export {
+  PortalProvider as BottomSheetPortalProvider,
+  type PortalItem,
+  usePortalComponent as useBottomSheetPortalComponent,
+  usePortal as useBottomSheetPortal,
+} from '@shaquillehinds/react-native-essentials';
+
 export * from './BottomSheet';
